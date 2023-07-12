5be2b6b129bcb1cce289b0fcb74d113998c35246
Abhisha Ashikbhai Thaker <ab570421@dal.ca>
2023-07-12 13:38:59 -0300

    comments added

--- A2/src/main/java/org/example/DB/User.java
+++ A2/src/main/java/org/example/DB/User.java
@@ -1,44 +1,88 @@
 package org.example.DB;
 
+/*
+To create an object of user with all the parameters
+ */
 public class User {
     private String ID;
     private String password;
     private String securityQuestion;
     private String securityAnswer;
 
+    /**
+     *
+     * @param Id Initializes Id passed while creating the object of the Id
+     * @param password Initializes password passed while creating the object of the user
+     * @param securityQuestion Initializes securityQuestion passed while creating the object of the user
+     * @param securityAnswer Initializes securityAnswer passed while creating the object of the user
+     */
     public User(String Id, String password,String securityQuestion,String securityAnswer){
         this.ID = Id;
         this.password = password;
         this.securityQuestion = securityQuestion;
         this.securityAnswer = securityAnswer;
     }
+
+    /**
+     *
+     * @param ID sets the userInput value of Id
+     */
     public void setID(String ID) {
         this.ID = ID;
     }
 
+    /**
+     *
+     * @param password sets the userInput value of Password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     *
+     * @param securityQuestion sets the userInput value of securityQuestion
+     */
     public void setSecurityQuestion(String securityQuestion) {
         this.securityQuestion = securityQuestion;
     }
 
+    /**
+     *
+     * @param securityAnswer sets the userInput value of securityAnswer
+     */
     public void setSecurityAnswer(String securityAnswer) {
         this.securityAnswer = securityAnswer;
     }
+
+    /**
+     *
+     * @return string ID
+     */
     public String getID() {
         return ID;
     }
 
+    /**
+     *
+     * @return String password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     *
+     * @return String securityQuestion
+     */
     public String getSecurityQuestion() {
         return securityQuestion;
     }
 
+    /**
+     *
+     * @return string securityAnswer
+     */
     public String getSecurityAnswer() {
         return securityAnswer;
     }
--- A2/src/main/java/org/example/DB/UserAuthentication.java
+++ A2/src/main/java/org/example/DB/UserAuthentication.java
@@ -10,14 +10,20 @@ import static org.example.DB.Main.USER_FILE_PATH;
 
 public class UserAuthentication {
     private Map<String, User> users = new HashMap<>();
-
     public UserAuthentication() {
     }
 
+    /**
+     *
+     * @param user passed the object of user and added it to Map users with key as Id and value as User object
+     */
     public void addUser(User user) {
         users.put(user.getID(), user);
     }
 
+    /**
+     * save the passed user object values to file
+     */
     public void saveUsersToFile() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(USER_FILE_PATH,true))) {
             for (User user : users.values()) {
@@ -30,6 +36,13 @@ public class UserAuthentication {
             e.printStackTrace();
         }
     }
+
+    /**
+     * checks the id and password match. If it matches it asks for security q and security answer
+     * @param ID userinput id
+     * @param password userinput password
+     * @return
+     */
     public boolean validateCredentials(String ID, String password) {
         User user = users.get(ID);
         String pass = user.getPassword();
@@ -40,10 +53,23 @@ public class UserAuthentication {
         System.out.println(value);
         return user != null && user.getPassword().equals(password);
     }
+
+    /**
+     * taking id as the key, security answer is validated
+     * @param ID userinput id
+     * @param answer userinput password
+     * @return
+     */
     public boolean validateSecurityAnswer(String ID, String answer) {
         User user = users.get(ID);
         return user != null && user.getSecurityAnswer().equals(answer);
     }
+
+    /**
+     *
+     * @param ID id - as a key passed to retrieve securityanswer
+     * @return returns the security answer that is in Id as key
+     */
     public String getSecurityQuestion(String ID){
         User user = users.get(ID);
         return user.getSecurityQuestion();
